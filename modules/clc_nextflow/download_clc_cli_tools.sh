4f6373ecea7f87b83d319af44e7037a66b2dc002
feiloo <105425286+feiloo@users.noreply.github.com>
2024-04-11 18:21:27 +0200

    build clc container with meson

---
+++ modules/clc_nextflow/download_clc_cli_tools.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -euo pipefail
+wget https://download.clcbio.com/CLCServerCommandLineTools/24.0.1/CLCServerCommandLineTools_24_0_1_64.sh -O third_party/CLCServerCommandLineTools_24_0_1_64.sh
