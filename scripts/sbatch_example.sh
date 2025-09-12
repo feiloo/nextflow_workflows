20cfc291867271d030053f4c991ed11e13e9c1b0
feiloo <105425286+feiloo@users.noreply.github.com>
2025-09-12 16:46:26 +0200

    add slurm example sbatch script

---
+++ scripts/sbatch_example.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --job-name='nextflow_workflow'
+#SBATCH --mem 16000 
+#SBATCH --time 240:00:00
+#SBATCH --requeue
+#SBATCH --export=ALL
+#SBATCH --error="slurm-%j.err"
+#SBATCH --output="slurm-%j.out"
+#SBATCH --nice=0
+
+# export RUNDIR_CUSTOM=$(pwd)
+export RUNDIR_CUSTOM=/home/pipeline_user/nextflow_calldir/
+
+# ensure the nextflow_workflows git repo is clean, and everything is commited
+pushd $NEXTFLOW_MODULES
+git diff --quiet && git diff --cached --quiet
+popd
+
+# use exec to propagate kill-signals, see: https://dhruveshp.com/blog/2021/signal-propagation-on-slurm/
+exec nextflow-25.04.3 run $NEXTFLOW_MODULES/ukb_main_workflow/ \
+        --workflow_variation align_interpret \
+	--samplesheet $RUNDIR_CUSTOM/samplesheet.csv \
+	--hash_db $RUNDIR_CUSTOM/md5sums.txt \
+	-c $NEXTFLOW_MODULES/ukb_main_workflow/user.config \
+	-profile standard \
+	-offline \
+	-resume \
+	--tag test,wes \
+        --bwa_tool bwa2
